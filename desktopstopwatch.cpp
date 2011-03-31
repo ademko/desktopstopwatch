f9697f7ddce81458c3de57003c1aec20fdfc1e1c
Aleksander Demko <ademko@gmail.com>
2011-03-30 23:02:49 -0500

    added some license updates and a README file

--- desktopstopwatch.cpp
+++ desktopstopwatch.cpp
@@ -2,16 +2,17 @@
 /**
 
   Desktop Stop Watch
-  Copyright (C) 2008 Aleksander B. Demko
+  Copyright (C) 2008-2011 Aleksander B. Demko
 
   This program is free software; you can redistribute it and/or modify
-  it under the terms of version 2 of the GNU General Public License
-  (and only version 2) as published by the Free Software Foundation.
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License version 2 for more details.
+  GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License along
   with this program; if not, write to the Free Software Foundation, Inc.,
